448b62baf5e0f08077fbf3c9b0805fc846850222
Vallek <vforval86@gmail.com>
2024-12-10 13:28:58 +0300

    Переверстка выпадающего меню + кнопки

--- source/scripts/dropdown-menu.js
+++ source/scripts/dropdown-menu.js
@@ -1,7 +1,7 @@
 'use stict';
 
-const menuButton = document.querySelector('.dropdown-menu__button');
-const menuDropdown = document.querySelector('.dropdown-menu__list');
+const menuButton = document.querySelector('.dropdown__button');
+const menuDropdown = document.querySelector('.dropdown__menu');
 menuButton.addEventListener('click', () => {
 	menuDropdown.classList.toggle('hidden');
 });
