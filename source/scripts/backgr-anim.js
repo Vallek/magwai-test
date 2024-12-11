691a4c1236aa5af558f90be4ca060cc2fcde68f1
Vallek <vforval86@gmail.com>
2024-12-11 15:02:18 +0300

    убрал лог

--- source/scripts/backgr-anim.js
+++ source/scripts/backgr-anim.js
@@ -7,7 +7,6 @@ const elementVisible = 116;
 
 	function scrollAnim() {
 		let elementTop = backImg.getBoundingClientRect().top;
-    console.log(elementTop);
 		if (elementTop <= windowHeight - elementVisible && elementTop > 0) {
       backImg.classList.remove("scroll-up");
 			backImg.classList.add("scroll-down");
