39b0273a107c48cf049d9013718b0d6c4c8ca92b
Vallek <22982571+Vallek@users.noreply.github.com>
2024-12-12 12:55:19 +0000

    Deploying to gh-pages from @ Vallek/magwai-test@c5ad2cee58cdd998017781a7a4303d1855229089 🚀

--- scripts/dropdown-menu.js
+++
@@ -1,6 +0,0 @@
-"use stict";
-const menuButton = document.querySelector(".dropdown__button");
-const menuDropdown = document.querySelector(".dropdown__menu");
-menuButton.addEventListener("click", () => {
-  menuDropdown.classList.toggle("hidden");
-});
